e56f5f843e98d9754752dcb73892bed0f1e8db8f
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-05-19 01:10:28 +0430

    fetch data from API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,22 +4,49 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      type: "hello world",
+      type: "1",
+      pokemonList: [],
     };
   }
+  onSelectTypeChange(event) {
+    // console.log("selectchange", event.target.value);
+    this.setState({ type: event.target.value });
+  }
+  onButtonClick(event) {
+    event.preventDefault();
+    // console.log(this.state.type + "this button clicked");
+    const API_URL = "https://pokeapi.co/api/v2/type/3/";
+    fetch(API_URL)
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        // console.log(data.pokemon);
+        this.setState({ pokemonList: data.pokemon });
+      })
+      .catch((error) => console.log(error));
+  }
   render() {
+    console.log(this.state);
     return (
       <div className="container app-container">
         <div>
           <h4>our small pokemon app</h4>
           <form>
             <label>choose your pokemon type</label>
-            <select>
+            <br />
+            <select onChange={this.onSelectTypeChange.bind(this)}>
               <option value="1">normal</option>
               <option value="2">fighting</option>
               <option value="3">flying</option>
+              <option value="4">poison</option>
             </select>
-            <button className="btn btn-success">search for type</button>
+            <button
+              onClick={this.onButtonClick.bind(this)}
+              className="btn btn-success"
+            >
+              search for type
+            </button>
           </form>
         </div>
       </div>
